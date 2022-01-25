2837529d7cd628c32754ef2d5ab4401b865b576f
yigityanik <yeyanik123@gmail.com>
2022-01-25 03:52:58 +0300

    shamrock: import kernel build flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,6 +45,17 @@ TARGET_NO_BOOTLOADER := true
 BUILD_BROKEN_DUP_RULES := true
 ALLOW_MISSING_DEPENDENCIES=true
 
+# Kernel
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlycon=msm_hsl_uart,0x78B0000 loop.max_part=7 androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_SOURCE := kernel/GM/shamrock
+TARGET_KERNEL_CONFIG := shamrock_defconfig
+TARGET_KERNEL_VERSION := 3.10
+
 # Security patch level
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
