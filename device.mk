5fd3dc61d056caed3bf3cec273fdf135832c2d6a
yigityanik <yeyanik123@gmail.com>
2022-01-26 02:41:11 +0300

    shamrock: build health hal

--- device.mk
+++ device.mk
@@ -141,6 +141,11 @@ PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
     libqti_vndfwk_detect.vendor
 
+# Health HAL
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service
+
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/seccomp/configstore@1.1.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/configstore@1.1.policy \
