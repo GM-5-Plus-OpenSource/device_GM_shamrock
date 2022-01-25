9b55db5eb940ed783021c638ecb63a4626d10cf5
yigityanik <yeyanik123@gmail.com>
2022-01-26 02:42:17 +0300

    shamrock: build cryptfs hal

--- device.mk
+++ device.mk
@@ -146,6 +146,10 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
     android.hardware.health@2.0-service
 
+# HW crypto
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
+
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/seccomp/configstore@1.1.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/configstore@1.1.policy \
