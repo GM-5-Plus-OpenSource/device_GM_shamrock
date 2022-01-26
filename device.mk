c63cd7a9bc57a3eafb35a0f0e4a2f3f6c59636a5
yigityanik <yeyanik123@gmail.com>
2022-01-26 03:47:24 +0300

    shamrock: import msm_irqbalance_little_big.conf

--- device.mk
+++ device.mk
@@ -10,6 +10,7 @@ TARGET_SCREEN_WIDTH := 1080
 # IRQ
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
+    $(LOCAL_PATH)/configs/msm_irqbalance_little_big.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance_little_big.conf
 
 # Keylayout
 PRODUCT_COPY_FILES += \
