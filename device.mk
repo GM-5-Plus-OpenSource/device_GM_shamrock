ad07b217b58df2b70f0b9a72ef870cfd9106b039
yigityanik <yeyanik123@gmail.com>
2022-01-26 03:14:02 +0300

    shamrock: build renderscript hal

--- device.mk
+++ device.mk
@@ -203,6 +203,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service-qti
 
+# RenderScript
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/seccomp/configstore@1.1.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/configstore@1.1.policy \
