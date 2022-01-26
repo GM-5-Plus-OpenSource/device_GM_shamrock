5c9ada740c3159bc99bc2cd3368e736aafd47c61
yigityanik <yeyanik123@gmail.com>
2022-01-26 03:30:04 +0300

    shamrock: build basic USB hal

--- device.mk
+++ device.mk
@@ -226,5 +226,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+# USB HAL
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.basic
+
 # Inherit vendor
 $(call inherit-product, vendor/GM/shamrock/shamrock-vendor.mk)
