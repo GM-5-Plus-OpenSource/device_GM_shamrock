a399dd7e8c736d906edb84fb0d57a029216b6bfb
yigityanik <yeyanik123@gmail.com>
2022-01-28 01:27:17 +0300

    shamrock: organize camera libraries

--- device.mk
+++ device.mk
@@ -111,12 +111,13 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.common@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
-    vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor \
-    Camera2
+    Camera2 \
+    libmm-qcamera \
+    libqomx_core
 
 # Configstore
 PRODUCT_PACKAGES += \
