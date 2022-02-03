98488f501de9ee4fd8ab5cbaf60dfb583016c4a0
yigityanik <yeyanik123@gmail.com>
2022-02-04 01:35:18 +0300

    shamrock: add required platform flags

--- device.mk
+++ device.mk
@@ -25,6 +25,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+#Soong
+PRODUCT_BOARD_PLATFORM := msm8952
+PRODUCT_USES_QCOM_HARDWARE := true
+
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
