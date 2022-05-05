7eef7b41f28d636a391146ae9cfb0527e1eaf25b
yigityanik <yeyanik123@gmail.com>
2022-05-06 02:19:49 +0300

    shamrock: set location permissions for gms 04-12 21:31:35.588  2674  5472 W ActivityManager: Appop Denial: Accessing service com.google.android.gms/com.google.android.location.internal.server.GoogleLocationService from pid=8601, uid=10134 requires appop COARSE_LOCATION
    ACCESS_COARSE_LOCATION and ACCESS_FINE_LOCATION permissions must be granted.

--- device.mk
+++ device.mk
@@ -306,6 +306,7 @@ PRODUCT_PACKAGES += \
 # Whitelisted app
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
+    $(LOCAL_PATH)/permissions/privapp-permissions-gms-location.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-gms-location.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
 # TextClassifier smart selection model files
